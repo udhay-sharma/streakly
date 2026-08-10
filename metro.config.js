646de5c8dc37897d55b7e9156831ef9b2956c1f5
Udhay Sharma <usdubey911@gmail.com>
2026-08-11 01:16:58 +0530

    add metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,10 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Add WASM support for expo-sqlite on web
+config.resolver.assetExts.push("wasm");
+
+
 module.exports = withNativeWind(config, {
   input: "./src/global.css",
 });
